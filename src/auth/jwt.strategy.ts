cc04fe42b8b62d0331c8e4529715a5496492a85e
narawit-tub <narawit.tub@outlook.co.th>
2026-02-03 14:37:32 +0700

    add missing files for jwt guard

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,23 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { jwtConstants } from './constants';
+import { User } from 'src/users/entities/user.entity';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: jwtConstants.secret,
+    });
+  }
+
+  validate(payload: {
+    sub: number;
+    username: string;
+  }): Pick<User, 'id' | 'username'> {
+    return { id: payload.sub, username: payload.username };
+  }
+}
